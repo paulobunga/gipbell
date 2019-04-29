1e05d76ef5a99438023d0f7c1683a01ae0dd55a0
giplash <anashkinviktar@gmail.com>
2019-04-29 08:38:00 +0300

    install deps

--- android/app/src/main/java/com/gipbell/MainApplication.java
+++ android/app/src/main/java/com/gipbell/MainApplication.java
@@ -3,6 +3,8 @@ package com.gipbell;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.voximplant.reactnative.VoxImplantReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -38,6 +40,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+            new VoxImplantReactPackage(),
           new ModuleRegistryAdapter(mModuleRegistryProvider)
       );
     }
